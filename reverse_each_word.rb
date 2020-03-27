b9ef0a140eb1414ef43409179c665ca79427f19c
Joseph E Auz <joseph.auz77@gmail.com>
2020-03-27 05:18:12 +0000

    Automatically backed up by Learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,9 @@
+
+def reverse_each_word(source_string)
+  new_array = []
+  new_array[0] = source_string
+  new_array[0].split(" ")
+  reverse_array = [] 
+  reverse_array = new_array.collect {|x| x.reverse!}
+  reverse_array
+end 
